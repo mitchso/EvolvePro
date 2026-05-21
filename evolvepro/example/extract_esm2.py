a08ef58b52fcb85bbd5afe7a2bb2388be3b1c37a
mitchso <mitchsybergolsen@gmail.com>
2026-05-21 09:14:20 -0500

    update examples

--- evolvepro/example/extract_esm2.py
+++ evolvepro/example/extract_esm2.py
@@ -5,6 +5,6 @@
 from evolvepro.plm.extract_esm2 import *
 
 extract_embeddings(model='esm2_t36_3B_UR50D',
-                   fasta_files=['WT.fasta', 'single_mutants.fasta'],    # make sure to include the wild-type sequence as well
-                   output_csv=f'embeddings_esm2_3b.csv',
+                   fasta_files=['outputs/WT.fasta', 'outputs/single_mutants.fasta'],    # make sure to include the wild-type sequence as well
+                   output_csv=f'outputs/embeddings_esm2_3b.csv',
                    seqs_per_batch=8)
